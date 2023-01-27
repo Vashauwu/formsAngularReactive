14bae744699c566ce430cb12d47ba1904af8820d
pc <adrian.valladarez@gmail.com>
2023-01-26 21:04:29 -0600

    complemento de formulario

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -4,21 +4,30 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import{FormQualityComponent} from './form-quality/form-quality.component';
+import { FormQualityComponent } from './form-quality/form-quality.component';
 import { FormRhComponent } from './form-rh/form-rh.component';
 import { CommonModule } from '@angular/common';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
-export type  FormType= 'rh' | 'quality';
+export type FormType = 'rh' | 'quality';
 @Component({
   standalone: true,
   selector: 'app-formulario',
-  imports: [CommonModule,MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule, ReactiveFormsModule,FormQualityComponent,FormRhComponent],
+  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule, ReactiveFormsModule, FormQualityComponent, FormRhComponent],
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent  {
+export class FormularioComponent {
 
   form: FormType = 'rh';
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   get showRh() {
     return this.form === 'rh';
@@ -31,4 +40,38 @@ export class FormularioComponent  {
   toggleEditor(type: FormType) {
     this.form = type;
   }
+
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
+  });
+
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+
 }
